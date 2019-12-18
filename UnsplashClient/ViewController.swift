69aa09775d487296baad2103094b3de1c011ea56
Никита Гагаринов <fliperstaa@gmail.com>
2019-12-18 14:37:38 +0300

    Initial commit

---
+++ UnsplashClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnsplashClient
+//
+//  Created by Никита Гагаринов on 18.12.2019.
+//  Copyright © 2019 nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
